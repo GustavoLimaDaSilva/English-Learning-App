310234c13d313c735cf97548cf2394be68e2161d
Gustavo Lima da Silva <gustavolimadasilva50@gmail.com>
2026-03-26 18:29:20 -0300

    fix: correct file extension for Opts export in types index

--- client/English-App/src/types/index.ts
+++ client/English-App/src/types/index.ts
@@ -2,6 +2,6 @@ export type StateSetter<T> = React.Dispatch<React.SetStateAction<T>>
 export type { DecksType, DeckType, FlashcardType } from "../../../../shared-types/deck.js"
 export type { LessonType } from "../../../../shared-types/lesson.js"
 export type { ProfileData } from "../../../../shared-types/user.js"
-export { Opts } from "../../../../shared-types/deck.js"
+export { Opts } from "../../../../shared-types/deck.ts"
 
 
